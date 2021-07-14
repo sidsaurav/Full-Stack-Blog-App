f6406ceeba7d0594911f044f0e780644bc95e198
Siddharth <sid.saurav11@gmail.com>
2021-07-14 12:07:46 +0530

    create header section

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,7 +3,15 @@ import './header.css'
 const Header = () => {
     return (
         <div className="header">
-            header
+            <div className = "headerTitles">
+                <span class="headerTitleSm">React & Node</span>
+                <span class="headerTitleLg">Blog</span>
+            </div>
+            <img
+            className="headerImg"
+            src="https://images.pexels.com/photos/459654/pexels-photo-459654.jpeg?cs=srgb&dl=pexels-pixabay-459654.jpg&fm=jpg"
+            alt="main"
+            />
         </div>
     )
 }
