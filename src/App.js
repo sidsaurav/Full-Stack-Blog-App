c45e6d54aeb2d9a4b49916b803824b3981ef24e0
Siddharth <sid.saurav11@gmail.com>
2021-07-14 18:35:35 +0530

    add register page and react router dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Topbar from "./components/topbar/Topbar"
 import Home from "./pages/home/Home"
 import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
 import Settings from "./pages/settings/Settings"
 import Single from "./pages/single/single"
 import Write from "./pages/write/Write"
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <Topbar />
-      <Login />
+      <Register />
     </>
   );
 }
