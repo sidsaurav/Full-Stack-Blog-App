f7053965ad1141e1d5a116d71c41858c4e97797f
Siddharth <sid.saurav11@gmail.com>
2021-07-15 15:06:59 +0530

    add user router

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv")
 const mongoose = require('mongoose');
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 app.use(express.json());
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO_URL, {
 }).then(console.log("Connected to mongoDB")).catch((err) => { console.log(err) });
 
 
+app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 
 app.listen("5000", () => {
