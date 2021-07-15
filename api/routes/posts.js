f763d069f3dd6a38fb31bd8625429aa8e7266cf4
Siddharth <sid.saurav11@gmail.com>
2021-07-15 16:13:57 +0530

    add post route

---
+++ api/routes/posts.js
@@ -0,0 +1,73 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const Post = require("../models/Post")
+
+//CREATE POST
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+    try {
+        const savedPost = await newPost.save();
+        res.status(200).json(savedPost)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+//UPDATE
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username) {
+            try {
+                const updatedPost = await Post.findByIdAndUpdate(
+                    req.params.id, {
+                    $set: req.body,
+                },
+                    { new: true }
+                );
+                res.status(200).json(updatedPost)
+            } catch (err) {
+                res.status(500).json(err)
+            }
+        }
+        else {
+            res.status(401).json("You can't update this post!")
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+    // res.json("Hi");
+});
+
+//DELETE
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.username === req.body.username) {
+            try {
+                await post.delete();
+                res.status(200).json("Post has been deleted")
+            } catch (err) {
+                res.status(500).json(err)
+            }
+        }
+        else {
+            res.status(401).json("You can't delete this post!")
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+//GET
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
